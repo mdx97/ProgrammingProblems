55071951ecf02de4d930689e4dc3cab5d1361899
mdx97 <mathewhorner123@gmail.com>
2019-03-11 15:58:38 -0500

    LeetCode #814

---
+++ LeetCode/p814.py
@@ -0,0 +1,27 @@
+from common import TreeNode
+
+class Solution(object):
+    def pruneTree(self, root):
+        self.pruneTreeRC(root)
+        return root
+    
+    def pruneTreeRC(self, root):
+        if root is None:
+            return False
+        if not root.left and not root.right:
+            return True if root.val == 1 else False
+        left_has_1 = self.pruneTreeRC(root.left)
+        right_has_1 = self.pruneTreeRC(root.right)
+        if not left_has_1:
+            root.left = None
+        if not right_has_1:
+            root.right = None
+        return root.val == 1 or left_has_1 or right_has_1
+
+sol = Solution()
+root = TreeNode(1)
+root.left = TreeNode(0)
+root.right = TreeNode(1)
+root.right.right = TreeNode(0)
+root.right.left = TreeNode(1)
+sol.pruneTree(root)
