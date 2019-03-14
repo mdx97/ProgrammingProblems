dfe0de955065cbb16643c635abb9711ed209c22b
mdx97 <mathewhorner123@gmail.com>
2019-03-14 14:25:51 -0500

    Altered LeetCode #771

--- LeetCode/p771.cpp
+++ LeetCode/p771.cpp
@@ -7,19 +7,19 @@ using namespace std;
 class Solution
 {
     public:
-    int numJewelsInStones(string J, string S)
-    {
-        unordered_set<char> jewels;
-        for (char c : J)
-            jewels.insert(c);
-        int count = 0;
-        for (char c : S)
+        int numJewelsInStones(string J, string S)
         {
-            if (jewels.find(c) != jewels.end())
-                count++;
+            unordered_set<char> jewels;
+            for (char c : J)
+                jewels.insert(c);
+            int count = 0;
+            for (char c : S)
+            {
+                if (jewels.find(c) != jewels.end())
+                    count++;
+            }
+            return count;
         }
-        return count;
-    }
 };
 
 int main()
